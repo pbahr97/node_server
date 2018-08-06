067488601b56683ceb49ea24a6befbc727fbd2c1
Pirmin Bahr <pirmin.bahr@helloguest.de>
2018-08-06 21:30:33 +0200

    adding dependencies

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+
+//different Http methods: post, get, put, delete, patch
+//getting information about some particular record
+app.get('/', (req, res) => {
+  //arrow function gets called everytime the specific route is visited
+  res.send({ hi: 'Hello World' });
+});
+
+// '/' which route to listen to  eg: '/Home'
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT);
